14ff9767283c68b0fce61bea72ed82195e7897a6
JAYASHREE668 <69155121+JAYASHREE668@users.noreply.github.com>
2020-08-03 23:43:27 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+**************************************************************************/
+public class Main
+{
+    public static void main(String args[])
+    {
+        int a=10,b=5;
+        System.out.print(a/b);
+        
+                                      
+    }
+}
